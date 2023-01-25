674da8605459926d24dd7a8daba75fa65aa0c275
Kevin Liu Rodrigues <kkevin.liu@protonmail.com>
2023-01-25 19:39:15 -0300

    fix movement system and add collisions system

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ use bevy::{prelude::*, sprite::MaterialMesh2dBundle, sprite::Mesh2dHandle};
 use std::f32::consts::PI;
 
 const BOUNDS: f32 = 1024.0;
-const SEGMENT_DIST: f32 = 32.0;
+const SEGMENT_SIZE: f32 = 32.0;
+const MOVEMENT_SPEED: f32 = 200.0;
+const TURN_SPEED: f32 = PI;
+
+enum CollisionLayer {
+    HEAD,
+    SEGMENT,
+    FOOD,
+    BOUNDS,
+}
 
 #[derive(Component)]
 struct MainCamera;
@@ -20,7 +29,13 @@ struct MinhocaHead {
 #[derive(Component)]
 struct MinhocaSegment;
 
-#[derive(Resource, Default, Deref, DerefMut)]
+#[derive(Component)]
+struct Collider {
+    layer: CollisionLayer,
+    radius: f32,
+}
+
+#[derive(Component, Default)]
 struct MinhocaSegments(Vec<Entity>);
 
 #[derive(Component)]
@@ -35,10 +50,11 @@ fn main() {
         .add_startup_system(setup_world)
         .add_startup_system(setup_minhoca)
         .add_startup_system(setup_bounds)
-        .insert_resource(MinhocaSegments::default())
+        // .insert_resource(MinhocaSegments::default())
         .insert_resource(MousePosition::default())
         .add_system(mouse_position_system.before(player_movement_system))
         .add_system(player_movement_system.before(camera_movement_system))
+        .add_system(collision_system)
         .add_system(camera_movement_system)
         .add_system(bevy::window::close_on_esc)
         .run();
@@ -56,7 +72,7 @@ fn setup_world(mut commands: Commands, asset_server: Res<AssetServer>) {
             commands.spawn((
                 SpriteBundle {
                     texture: bg_tile.clone(),
-                    transform: Transform::from_xyz((i as f32) * width, (j as f32) * height, 1.0),
+                    transform: Transform::from_xyz((i as f32) * width, (j as f32) * height, 0.0),
                     ..default()
                 },
                 BackgroundTile,
@@ -70,61 +86,75 @@ fn setup_bounds(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // Torus is a 3d shape, so we need to ratate it to face the camera.
     let torus_mesh = Mesh::from(shape::Torus {
         radius: BOUNDS + 16.,
         ring_radius: 16.,
         subdivisions_segments: 128,
         subdivisions_sides: 4,
     });
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle::from(meshes.add(torus_mesh)),
-        material: materials.add(ColorMaterial::from(Color::PINK)),
-        transform: Transform::from_xyz(0., 0., 1.0).with_rotation(Quat::from_rotation_x(PI / 2.)),
-        ..default()
-    });
+    // Torus is a 3d shape, so we need to ratate it to face the camera.
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle::from(meshes.add(torus_mesh)),
+            material: materials.add(ColorMaterial::from(Color::PINK)),
+            transform: Transform::from_xyz(0., 0., 1.0)
+                .with_rotation(Quat::from_rotation_x(PI / 2.)),
+            ..default()
+        },
+        Collider {
+            layer: CollisionLayer::BOUNDS,
+            radius: BOUNDS,
+        },
+    ));
 }
 
-fn setup_minhoca(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut segments: ResMut<MinhocaSegments>,
-) {
-    *segments = MinhocaSegments(vec![
-        commands
-            .spawn((
-                SpriteBundle {
-                    texture: asset_server.load("minhoca_head.png"),
-                    transform: Transform::from_xyz(0.0, 0.0, 2.0),
-                    ..default()
-                },
-                MinhocaHead {
-                    movement_speed: 200.0,
-                    rotation_speed: f32::to_radians(180.0),
-                },
-                MinhocaSegment,
-            ))
-            .id(),
-        spawn_segment(&mut commands, &asset_server, 0., -1. * SEGMENT_DIST),
-        spawn_segment(&mut commands, &asset_server, 0., -2. * SEGMENT_DIST),
-        spawn_segment(&mut commands, &asset_server, 0., -3. * SEGMENT_DIST),
-        spawn_segment(&mut commands, &asset_server, 0., -4. * SEGMENT_DIST),
-        spawn_segment(&mut commands, &asset_server, 0., -5. * SEGMENT_DIST),
-    ]);
+fn setup_minhoca(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let initial_head_transform = Transform::from_xyz(0., 0., 2.);
+    let mut minhoca = vec![commands
+        .spawn((
+            SpriteBundle {
+                texture: asset_server.load("minhoca_head.png"),
+                transform: initial_head_transform,
+                ..default()
+            },
+            MinhocaHead {
+                movement_speed: MOVEMENT_SPEED,
+                rotation_speed: TURN_SPEED,
+            },
+            Collider {
+                layer: CollisionLayer::HEAD,
+                radius: SEGMENT_SIZE,
+            },
+            MinhocaSegment,
+        ))
+        .id()];
+    for i in 1..=10 {
+        minhoca.push(spawn_segment(
+            &mut commands,
+            &asset_server,
+            SEGMENT_SIZE,
+            Vec2::from((0., -(i as f32) * SEGMENT_SIZE)),
+        ));
+    }
+    commands.spawn(MinhocaSegments(minhoca));
 }
 
 fn spawn_segment(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
-    x: f32,
-    y: f32,
+    size: f32,
+    pos: Vec2,
 ) -> Entity {
     commands
         .spawn((
             SpriteBundle {
                 texture: asset_server.load("minhoca_segment.png"),
-                transform: Transform::from_xyz(x, y, 2.0),
+                transform: Transform::from_xyz(pos.x, pos.y, 100.0),
                 ..default()
+            },
+            Collider {
+                layer: CollisionLayer::SEGMENT,
+                radius: size,
             },
             MinhocaSegment,
         ))
@@ -153,13 +183,16 @@ fn mouse_position_system(
 fn player_movement_system(
     time: Res<Time>,
     head: Query<&MinhocaHead>,
-    mut minhoca: Query<(&mut Transform, &MinhocaSegment)>,
+    mut segment_transforms: Query<&mut Transform, With<MinhocaSegment>>,
+    minhoca_segments: Query<&MinhocaSegments>,
     mouse_pos: Res<MousePosition>,
 ) {
     let delta = time.delta_seconds();
-    let mut segments = minhoca.iter_mut();
-    let (mut head_transform, _) = segments.next().unwrap();
+    let mut entities = minhoca_segments.single().0.iter();
 
+    let mut head_transform = segment_transforms
+        .get_mut(*entities.next().unwrap())
+        .unwrap();
     let rotation_sign = if let Some(mouse) = mouse_pos.0 {
         let movement_direction = (head_transform.rotation * Vec3::Y).truncate();
         let mouse_direction = mouse - head_transform.translation.truncate();
@@ -169,28 +202,69 @@ fn player_movement_system(
         0.0
     };
 
-    let minhoca_head = head.single();
-    head_transform.rotate_z(rotation_sign * minhoca_head.rotation_speed * delta);
-    let dx = minhoca_head.movement_speed * delta;
+    // Move the head
+    let movement_speed = head.single().movement_speed;
+    let rotation_speed = head.single().rotation_speed;
+    let dx = movement_speed * delta;
     let dv = head_transform.rotation * Vec3::Y;
+
+    head_transform.rotate_z(rotation_sign * rotation_speed * delta);
     head_transform.translation += dx * dv;
 
-    let mut head_pos = head_transform.translation;
-    for (mut transform, _) in segments {
-        let old_pos = transform.translation;
-        let direction = old_pos - head_pos;
-        let new_pos = head_pos + direction.normalize() * f32::min(SEGMENT_DIST, direction.length());
-        transform.translation = new_pos;
-        head_pos = new_pos;
-    }
+    let mut head_transform = head_transform.clone();
+    entities.for_each(|e| {
+        let mut seg_transform = segment_transforms.get_mut(*e).unwrap();
+        let direction = seg_transform.translation - head_transform.translation;
+        let new_translation = head_transform.translation
+            + direction.normalize() * f32::min(SEGMENT_SIZE, direction.length());
+        let new_transform =
+            Transform::from_translation(new_translation).with_rotation(seg_transform.rotation);
+        *seg_transform = new_transform;
+        head_transform = new_transform;
+    });
 }
 
 fn camera_movement_system(
-    q_player: Query<(&Transform, With<MinhocaHead>), Without<MainCamera>>,
+    q_player: Query<(&Transform, &MinhocaHead), Without<MainCamera>>,
     mut q_camera: Query<&mut Transform, With<MainCamera>>,
 ) {
-    let transform = q_player.single().0;
+    let head_transform = q_player.single().0;
     let mut cam_transform = q_camera.single_mut();
-    cam_transform.translation.x = transform.translation.x;
-    cam_transform.translation.y = transform.translation.y;
+    cam_transform.translation.x = head_transform.translation.x;
+    cam_transform.translation.y = head_transform.translation.y;
+}
+
+fn check_collision_circles(r1: f32, c1: Vec3, r2: f32, c2: Vec3) -> bool {
+    let distance = (c1.x - c2.x).powi(2) + (c1.y - c2.y).powi(2);
+    if distance <= (r1 + r2).powi(2) {
+        return true;
+    }
+    false
+}
+
+fn collision_system(
+    heads: Query<(&Transform, &Collider), With<MinhocaHead>>,
+    colliders: Query<(&Transform, &Collider), (With<Collider>, Without<MinhocaHead>)>,
+    mut commands: Commands,
+) {
+    let (head_pos, head_collider) = heads.single();
+    let r1 = head_collider.radius;
+    let c1 = head_pos.translation;
+
+    let mut s = 0;
+    for (transform, collider) in colliders.iter().filter(|e| match e.1.layer {
+        CollisionLayer::HEAD => false,
+        CollisionLayer::SEGMENT => false,
+        CollisionLayer::FOOD => true,
+        CollisionLayer::BOUNDS => true,
+    }) {
+        let r2 = collider.radius;
+        let c2 = transform.translation;
+        if check_collision_circles(r1, c1, r2, c2) {
+            s += 1;
+        }
+    }
+    // println!("Head collided with {:?} elements this frame.", s);
+    // println!("HEAD AT: {:?}", head_pos.translation);
+    // println!("SPEED: {:?}", minhoca_head.movement_speed);
 }
