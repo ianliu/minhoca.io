ff3001dde6a3f5df6b873c601bad1117b57f7df6
Ian Liu Rodrigues <ian.liu88@gmail.com>
2023-02-26 13:53:42 -0300

    Initial commit

---
+++ common.js
@@ -0,0 +1,37 @@
+const pc = new RTCPeerConnection({
+  iceServers: [ { urls: "stun:stun.l.google.com:19302" } ]
+});
+
+let chan = null;
+
+let my_payload = {description: null, candidates: []};
+
+pc.onicecandidate = event => {
+  if (event.candidate) {
+    console.log(event.candidate);
+    my_payload.candidates.push(event.candidate.toJSON());
+    setValue("my_payload", my_payload);
+  }
+};
+
+function setValue(id, obj) {
+  document.getElementById(id).textContent = JSON.stringify(obj);
+}
+
+function getValue(id) {
+  return JSON.parse(document.getElementById(id).value);
+}
+
+async function addCandidates(candidates) {
+  for (const candidate of candidates)
+    await pc.addIceCandidate(candidate);
+}
+
+function setup_channel(who, chan) {
+  chan.onopen = event => console.log({ onopen: event });
+  chan.onclose = event => console.log({ onclose: event });
+  chan.onmessage = event => {
+    const chatlog = document.getElementById("chatlog");
+    chatlog.textContent += who + ": " + event.data + "\n";
+  };
+}
